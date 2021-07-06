82513a30de92dc73620c7b57e94bb8a592677be5
Michail Golubjev <xopkop@gmail.com>
2021-07-06 23:40:16 +0200

    Add project files.

---
+++ AuPolylineDBX/AuPolyline.cpp
@@ -0,0 +1,414 @@
+// (C) Copyright 2002-2007 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+//-----------------------------------------------------------------------------
+//----- AuPolyline.cpp : Implementation of AuPolyline
+//-----------------------------------------------------------------------------
+#include "StdAfx.h"
+#include "AuPolyline.h"
+
+//-----------------------------------------------------------------------------
+Adesk::UInt32 AuPolyline::kCurrentVersionNumber = 1;
+
+//-----------------------------------------------------------------------------
+ACRX_DXF_DEFINE_MEMBERS(
+	AuPolyline, AcDbPolyline,
+	AcDb::kDHL_CURRENT, AcDb::kMReleaseCurrent,
+	AcDbProxyEntity::kNoOperation, AUPOLYLINE,
+	AUPOLYLINEDBXAPP
+	| Product Desc : A description for your object
+	| Company : Your company name
+	| WEB Address : Your company WEB site address
+)
+
+//-----------------------------------------------------------------------------
+AuPolyline::AuPolyline() : AcDbPolyline()
+{}
+
+AuPolyline::~AuPolyline()
+{}
+
+//-----------------------------------------------------------------------------
+//----- AcDbObject protocols
+//- Dwg Filing protocol
+Acad::ErrorStatus AuPolyline::dwgOutFields(AcDbDwgFiler * pFiler) const
+{
+	assertReadEnabled();
+	//----- Save parent class information first.
+	Acad::ErrorStatus es = AcDbPolyline::dwgOutFields(pFiler);
+	if (es != Acad::eOk)
+		return (es);
+	//----- Object version number needs to be saved first
+	if ((es = pFiler->writeUInt32(AuPolyline::kCurrentVersionNumber)) != Acad::eOk)
+		return (es);
+	//----- Output params
+	//.....
+
+	return (pFiler->filerStatus());
+}
+
+Acad::ErrorStatus AuPolyline::dwgInFields(AcDbDwgFiler * pFiler)
+{
+	assertWriteEnabled();
+	//----- Read parent class information first.
+	Acad::ErrorStatus es = AcDbPolyline::dwgInFields(pFiler);
+	if (es != Acad::eOk)
+		return (es);
+	//----- Object version number needs to be read first
+	Adesk::UInt32 version = 0;
+	if ((es = pFiler->readUInt32(&version)) != Acad::eOk)
+		return (es);
+	if (version > AuPolyline::kCurrentVersionNumber)
+		return (Acad::eMakeMeProxy);
+	//- Uncomment the 2 following lines if your current object implementation cannot
+	//- support previous version of that object.
+	//if ( version < AuPolyline::kCurrentVersionNumber )
+	//	return (Acad::eMakeMeProxy) ;
+	//----- Read params
+	//.....
+
+	return (pFiler->filerStatus());
+}
+
+//-----------------------------------------------------------------------------
+//----- AcDbEntity protocols
+Adesk::Boolean AuPolyline::subWorldDraw(AcGiWorldDraw * mode)
+{
+	assertReadEnabled();
+	//Call base class first
+	AcDbPolyline::subWorldDraw(mode);
+	double szRef = 5.0;
+
+	//===================================================================
+	//DIRECTION AND VERTEX NUMBERING
+	int signal = 1;
+	double ht2 = szRef / 4.0;
+
+	for (uint i = 0; i < numVerts(); i++)
+	{
+		AcGePoint3d pti;
+		this->getPointAt(i, pti);
+
+		//Draw vertex text
+		CString strNum;
+		strNum.Format(_T("%d"), i);
+		AcGePoint3d ptTxt = pti + (AcGeVector3d::kXAxis * ht2) + (AcGeVector3d::kYAxis * ht2);
+		mode->subEntityTraits().setColor(256); //ByLayer
+		mode->geometry().text(ptTxt, AcGeVector3d::kZAxis, AcGeVector3d::kXAxis, ht2, 1.0, 0.0, strNum);
+
+		//Arrow direction
+		AcGePoint3d ptj;
+		this->getPointAt(i < (numVerts() - 1) ? (i + 1) : 0, ptj);
+		AcGeVector3d dir = (ptj - pti).normalize();
+
+		//Side perpendicular vectors
+		AcGeVector3d perp = dir;
+		perp.rotateBy(3.141592 / 2.0, AcGeVector3d::kZAxis);
+		AcGePoint3d pt1 = ptj - (dir * ht2) + (perp * (ht2 / 4.0));
+		AcGePoint3d pt2 = ptj - (dir * ht2) - (perp * (ht2 / 4.0));
+		AcGePoint3d pts[3];
+		pts[0] = ptj;
+		pts[1] = pt1;
+		pts[2] = pt2;
+
+		//Draw arrow polygon
+		mode->subEntityTraits().setFillType(kAcGiFillAlways);
+		mode->subEntityTraits().setColor(1);//Red
+		mode->geometry().polygon(3, pts);
+		mode->subEntityTraits().setFillType(kAcGiFillNever);
+	}
+
+	//------ Returning Adesk::kFalse here will force viewportDraw() call
+	return (Adesk::kTrue);
+}
+
+void AuPolyline::subViewportDraw(AcGiViewportDraw * mode)
+{
+	assertReadEnabled();
+	AcDbPolyline::subViewportDraw(mode);
+}
+
+Adesk::UInt32 AuPolyline::subViewportDrawLogicalFlags(AcGiViewportDraw * vd)
+{
+	assertReadEnabled();
+	return (AcDbPolyline::subViewportDrawLogicalFlags(vd));
+}
+
+Adesk::UInt32 AuPolyline::subSetAttributes(AcGiDrawableTraits * traits)
+{
+	assertReadEnabled();
+	return (AcDbPolyline::subSetAttributes(traits));
+}
+
+//- Osnap points protocol
+Acad::ErrorStatus AuPolyline::subGetOsnapPoints(
+	AcDb::OsnapMode osnapMode,
+	Adesk::GsMarker gsSelectionMark,
+	const AcGePoint3d & pickPoint,
+	const AcGePoint3d & lastPoint,
+	const AcGeMatrix3d & viewXform,
+	AcGePoint3dArray & snapPoints,
+	AcDbIntArray & geomIds) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::subGetOsnapPoints(osnapMode, gsSelectionMark, pickPoint, lastPoint, viewXform, snapPoints, geomIds));
+}
+
+Acad::ErrorStatus AuPolyline::subGetOsnapPoints(
+	AcDb::OsnapMode osnapMode,
+	Adesk::GsMarker gsSelectionMark,
+	const AcGePoint3d & pickPoint,
+	const AcGePoint3d & lastPoint,
+	const AcGeMatrix3d & viewXform,
+	AcGePoint3dArray & snapPoints,
+	AcDbIntArray & geomIds,
+	const AcGeMatrix3d & insertionMat) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::subGetOsnapPoints(osnapMode, gsSelectionMark, pickPoint, lastPoint, viewXform, snapPoints, geomIds, insertionMat));
+}
+
+//- Grip points protocol
+Acad::ErrorStatus AuPolyline::subGetGripPoints(
+	AcGePoint3dArray & gripPoints, AcDbIntArray & osnapModes, AcDbIntArray & geomIds
+) const
+{
+	assertReadEnabled();
+	//----- This method is never called unless you return eNotImplemented 
+	//----- from the new getGripPoints() method below (which is the default implementation)
+
+	return (AcDbPolyline::subGetGripPoints(gripPoints, osnapModes, geomIds));
+}
+
+Acad::ErrorStatus AuPolyline::subMoveGripPointsAt(const AcDbIntArray & indices, const AcGeVector3d & offset)
+{
+	assertWriteEnabled();
+	//----- This method is never called unless you return eNotImplemented 
+	//----- from the new moveGripPointsAt() method below (which is the default implementation)
+
+	return (AcDbPolyline::subMoveGripPointsAt(indices, offset));
+}
+
+Acad::ErrorStatus AuPolyline::subGetGripPoints(
+	AcDbGripDataPtrArray & grips, const double curViewUnitSize, const int gripSize,
+	const AcGeVector3d & curViewDir, const int bitflags
+) const
+{
+	assertReadEnabled();
+
+	//----- If you return eNotImplemented here, that will force AutoCAD to call
+	//----- the older getGripPoints() implementation. The call below may return
+	//----- eNotImplemented depending of your base class.
+	return (AcDbPolyline::subGetGripPoints(grips, curViewUnitSize, gripSize, curViewDir, bitflags));
+}
+
+Acad::ErrorStatus AuPolyline::subMoveGripPointsAt(
+	const AcDbVoidPtrArray & gripAppData, const AcGeVector3d & offset,
+	const int bitflags
+)
+{
+	assertWriteEnabled();
+
+	//----- If you return eNotImplemented here, that will force AutoCAD to call
+	//----- the older getGripPoints() implementation. The call below may return
+	//----- eNotImplemented depending of your base class.
+	return (AcDbPolyline::subMoveGripPointsAt(gripAppData, offset, bitflags));
+}
+
+//-----------------------------------------------------------------------------
+//----- AcDbCurve protocols
+//- Curve property tests.
+Adesk::Boolean AuPolyline::isClosed() const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::isClosed());
+}
+
+Adesk::Boolean AuPolyline::isPeriodic() const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::isPeriodic());
+}
+
+//- Get planar and start/end geometric properties.
+Acad::ErrorStatus AuPolyline::getStartParam(double& param) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getStartParam(param));
+}
+
+Acad::ErrorStatus AuPolyline::getEndParam(double& param) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getEndParam(param));
+}
+
+Acad::ErrorStatus AuPolyline::getStartPoint(AcGePoint3d & point) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getStartPoint(point));
+}
+
+Acad::ErrorStatus AuPolyline::getEndPoint(AcGePoint3d & point) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getEndPoint(point));
+}
+
+//- Conversions to/from parametric/world/distance.
+Acad::ErrorStatus AuPolyline::getPointAtParam(double param, AcGePoint3d & point) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getPointAtParam(param, point));
+}
+
+Acad::ErrorStatus AuPolyline::getParamAtPoint(const AcGePoint3d & point, double& param) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getParamAtPoint(point, param));
+}
+
+Acad::ErrorStatus AuPolyline::getDistAtParam(double param, double& dist) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getDistAtParam(param, dist));
+}
+
+Acad::ErrorStatus AuPolyline::getParamAtDist(double dist, double& param) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getParamAtDist(dist, param));
+}
+
+Acad::ErrorStatus AuPolyline::getDistAtPoint(const AcGePoint3d & point, double& dist) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getDistAtPoint(point, dist));
+}
+
+Acad::ErrorStatus AuPolyline::getPointAtDist(double dist, AcGePoint3d & point) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getPointAtDist(dist, point));
+}
+
+//- Derivative information.
+Acad::ErrorStatus AuPolyline::getFirstDeriv(double param, AcGeVector3d & firstDeriv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getFirstDeriv(param, firstDeriv));
+}
+
+Acad::ErrorStatus AuPolyline::getFirstDeriv(const AcGePoint3d & point, AcGeVector3d & firstDeriv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getFirstDeriv(point, firstDeriv));
+}
+
+Acad::ErrorStatus AuPolyline::getSecondDeriv(double param, AcGeVector3d & secDeriv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getSecondDeriv(param, secDeriv));
+}
+
+Acad::ErrorStatus AuPolyline::getSecondDeriv(const AcGePoint3d & point, AcGeVector3d & secDeriv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getSecondDeriv(point, secDeriv));
+}
+
+//- Closest point on curve.
+Acad::ErrorStatus AuPolyline::getClosestPointTo(const AcGePoint3d & givenPnt, AcGePoint3d & pointOnCurve, Adesk::Boolean extend /*=Adesk::kFalse*/) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getClosestPointTo(givenPnt, pointOnCurve, extend));
+}
+
+Acad::ErrorStatus AuPolyline::getClosestPointTo(const AcGePoint3d & givenPnt, const AcGeVector3d & direction, AcGePoint3d & pointOnCurve, Adesk::Boolean extend /*=Adesk::kFalse*/) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getClosestPointTo(givenPnt, direction, pointOnCurve, extend));
+}
+
+//- Get a projected copy of the curve.
+Acad::ErrorStatus AuPolyline::getOrthoProjectedCurve(const AcGePlane & plane, AcDbCurve * &projCrv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getOrthoProjectedCurve(plane, projCrv));
+}
+
+Acad::ErrorStatus AuPolyline::getProjectedCurve(const AcGePlane & plane, const AcGeVector3d & projDir, AcDbCurve * &projCrv) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getProjectedCurve(plane, projDir, projCrv));
+}
+
+//- Get offset, spline and split copies of the curve.
+Acad::ErrorStatus AuPolyline::getOffsetCurves(double offsetDist, AcDbVoidPtrArray & offsetCurves) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getOffsetCurves(offsetDist, offsetCurves));
+}
+
+Acad::ErrorStatus AuPolyline::getOffsetCurvesGivenPlaneNormal(const AcGeVector3d & normal, double offsetDist, AcDbVoidPtrArray & offsetCurves) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getOffsetCurvesGivenPlaneNormal(normal, offsetDist, offsetCurves));
+}
+
+Acad::ErrorStatus AuPolyline::getSpline(AcDbSpline * &spline) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getSpline(spline));
+}
+
+Acad::ErrorStatus AuPolyline::getSplitCurves(const AcGeDoubleArray & params, AcDbVoidPtrArray & curveSegments) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getSplitCurves(params, curveSegments));
+}
+
+Acad::ErrorStatus AuPolyline::getSplitCurves(const AcGePoint3dArray & points, AcDbVoidPtrArray & curveSegments) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getSplitCurves(points, curveSegments));
+}
+
+//- Extend the curve.
+Acad::ErrorStatus AuPolyline::extend(double newParam)
+{
+	assertReadEnabled();
+	return (AcDbPolyline::extend(newParam));
+}
+
+Acad::ErrorStatus AuPolyline::extend(Adesk::Boolean extendStart, const AcGePoint3d & toPoint)
+{
+	assertReadEnabled();
+	return (AcDbPolyline::extend(extendStart, toPoint));
+}
+
+//- Area calculation.
+Acad::ErrorStatus AuPolyline::getArea(double& area) const
+{
+	assertReadEnabled();
+	return (AcDbPolyline::getArea(area));
+}
+
---
+++ AuPolylineDBX/AuPolyline.h
@@ -0,0 +1,147 @@
+// (C) Copyright 2002-2007 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+//-----------------------------------------------------------------------------
+//----- AuPolyline.h : Declaration of the AuPolyline
+//-----------------------------------------------------------------------------
+#pragma once
+
+#ifdef AUPOLYLINEDBX_MODULE
+#define DLLIMPEXP __declspec(dllexport)
+#else
+//----- Note: we don't use __declspec(dllimport) here, because of the
+//----- "local vtable" problem with msvc. If you use __declspec(dllimport),
+//----- then, when a client dll does a new on the class, the object's
+//----- vtable pointer points to a vtable allocated in that client
+//----- dll. If the client dll then passes the object to another dll,
+//----- and the client dll is then unloaded, the vtable becomes invalid
+//----- and any virtual calls on the object will access invalid memory.
+//-----
+//----- By not using __declspec(dllimport), we guarantee that the
+//----- vtable is allocated in the server dll during the ctor and the
+//----- client dll does not overwrite the vtable pointer after calling
+//----- the ctor. And, since we expect the server dll to remain in
+//----- memory indefinitely, there is no problem with vtables unexpectedly
+//----- going away.
+#define DLLIMPEXP
+#endif
+
+//-----------------------------------------------------------------------------
+#include "dbpl.h"
+
+//-----------------------------------------------------------------------------
+class DLLIMPEXP AuPolyline : public AcDbPolyline {
+
+public:
+	ACRX_DECLARE_MEMBERS(AuPolyline) ;
+
+protected:
+	static Adesk::UInt32 kCurrentVersionNumber ;
+
+public:
+	AuPolyline () ;
+	virtual ~AuPolyline () ;
+
+	//----- AcDbObject protocols
+	//- Dwg Filing protocol
+	virtual Acad::ErrorStatus dwgOutFields (AcDbDwgFiler *pFiler) const ;
+	virtual Acad::ErrorStatus dwgInFields (AcDbDwgFiler *pFiler) ;
+
+	//----- AcDbEntity protocols
+	//- Graphics protocol
+protected:
+	virtual void subViewportDraw (AcGiViewportDraw *mode) ;
+	virtual Adesk::UInt32 subViewportDrawLogicalFlags (AcGiViewportDraw *vd) ;
+	virtual Adesk::Boolean subWorldDraw (AcGiWorldDraw *mode) ;
+	virtual Adesk::UInt32 subSetAttributes (AcGiDrawableTraits *traits) ;
+
+	//- Osnap points protocol
+public:
+	virtual Acad::ErrorStatus subGetOsnapPoints (
+		AcDb::OsnapMode osnapMode,
+		Adesk::GsMarker gsSelectionMark,
+		const AcGePoint3d &pickPoint,
+		const AcGePoint3d &lastPoint,
+		const AcGeMatrix3d &viewXform,
+		AcGePoint3dArray &snapPoints,
+		AcDbIntArray &geomIds) const ;
+	virtual Acad::ErrorStatus subGetOsnapPoints (
+		AcDb::OsnapMode osnapMode,
+		Adesk::GsMarker gsSelectionMark,
+		const AcGePoint3d &pickPoint,
+		const AcGePoint3d &lastPoint,
+		const AcGeMatrix3d &viewXform,
+		AcGePoint3dArray &snapPoints,
+		AcDbIntArray &geomIds,
+		const AcGeMatrix3d &insertionMat) const ;
+
+	//- Grip points protocol
+	virtual Acad::ErrorStatus subGetGripPoints (AcGePoint3dArray &gripPoints, AcDbIntArray &osnapModes, AcDbIntArray &geomIds) const ;
+	virtual Acad::ErrorStatus subMoveGripPointsAt (const AcDbIntArray &indices, const AcGeVector3d &offset) ;
+	virtual Acad::ErrorStatus subGetGripPoints (
+		AcDbGripDataPtrArray &grips, const double curViewUnitSize, const int gripSize, 
+		const AcGeVector3d &curViewDir, const int bitflags) const ;
+	virtual Acad::ErrorStatus subMoveGripPointsAt (const AcDbVoidPtrArray &gripAppData, const AcGeVector3d &offset, const int bitflags) ;
+
+	//----- AcDbCurve protocols
+	//- Curve property tests.
+	virtual Adesk::Boolean isClosed () const ;
+	virtual Adesk::Boolean isPeriodic () const ;
+	//- Get planar and start/end geometric properties.
+	virtual Acad::ErrorStatus getStartParam (double &param) const ;
+	virtual Acad::ErrorStatus getEndParam (double &param) const ;
+	virtual Acad::ErrorStatus getStartPoint (AcGePoint3d &point) const ;
+	virtual Acad::ErrorStatus getEndPoint (AcGePoint3d &point) const ;
+	//- Conversions to/from parametric/world/distance.
+	virtual Acad::ErrorStatus getPointAtParam (double param, AcGePoint3d &point) const ;
+	virtual Acad::ErrorStatus getParamAtPoint (const AcGePoint3d &point, double &param) const ;
+	virtual Acad::ErrorStatus getDistAtParam (double param, double &dist) const ;
+	virtual Acad::ErrorStatus getParamAtDist (double dist, double &param) const ;
+	virtual Acad::ErrorStatus getDistAtPoint (const AcGePoint3d &point , double &dist) const ;
+	virtual Acad::ErrorStatus getPointAtDist (double dist, AcGePoint3d &point) const ;
+	//- Derivative information.
+	virtual Acad::ErrorStatus getFirstDeriv (double param, AcGeVector3d &firstDeriv) const ;
+	virtual Acad::ErrorStatus getFirstDeriv  (const AcGePoint3d &point, AcGeVector3d &firstDeriv) const ;
+	virtual Acad::ErrorStatus getSecondDeriv (double param, AcGeVector3d &secDeriv) const ;
+	virtual Acad::ErrorStatus getSecondDeriv (const AcGePoint3d &point, AcGeVector3d &secDeriv) const ;
+	//- Closest point on curve.
+	virtual Acad::ErrorStatus getClosestPointTo (const AcGePoint3d &givenPnt, AcGePoint3d &pointOnCurve, Adesk::Boolean extend =Adesk::kFalse) const ;
+	virtual Acad::ErrorStatus getClosestPointTo (const AcGePoint3d &givenPnt, const AcGeVector3d &direction, AcGePoint3d &pointOnCurve, Adesk::Boolean extend =Adesk::kFalse) const ;
+	//- Get a projected copy of the curve.
+	virtual Acad::ErrorStatus getOrthoProjectedCurve (const AcGePlane &plane, AcDbCurve *&projCrv) const ;
+	virtual Acad::ErrorStatus getProjectedCurve (const AcGePlane &plane, const AcGeVector3d &projDir, AcDbCurve *&projCrv) const ;
+	//- Get offset, spline and split copies of the curve.
+	virtual Acad::ErrorStatus getOffsetCurves (double offsetDist, AcDbVoidPtrArray &offsetCurves) const ;
+	virtual Acad::ErrorStatus getOffsetCurvesGivenPlaneNormal (const AcGeVector3d &normal, double offsetDist, AcDbVoidPtrArray &offsetCurves) const ;
+	virtual Acad::ErrorStatus getSpline (AcDbSpline *&spline) const ;
+	virtual Acad::ErrorStatus getSplitCurves (const AcGeDoubleArray &params, AcDbVoidPtrArray &curveSegments) const ;
+	virtual Acad::ErrorStatus getSplitCurves (const AcGePoint3dArray &points, AcDbVoidPtrArray &curveSegments) const ;
+	//- Extend the curve.
+	virtual Acad::ErrorStatus extend (double newParam) ;
+	virtual Acad::ErrorStatus extend (Adesk::Boolean extendStart, const AcGePoint3d &toPoint) ;
+	//- Area calculation.
+	virtual Acad::ErrorStatus getArea (double &area) const ;
+
+} ;
+
+#ifdef AUPOLYLINEDBX_MODULE
+ACDB_REGISTER_OBJECT_ENTRY_AUTO(AuPolyline)
+#endif
